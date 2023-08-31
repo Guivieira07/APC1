915d7d9c7d0ec1db1911d02bb298ab92965066af
Guivieira07 <142457716+Guivieira07@users.noreply.github.com>
2023-08-31 00:50:15 +0000

    Codigo da aula02 e da pratica01

---
+++ praticas/pratica01/questao02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+  int altura = 14;
+  int base = 7;
+  
+  float area = altura * base /2.0f;
+  printf("a area do triângulo é %f\n", area);
+  return 0;
+}
---
+++ praticas/pratica01/questao03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+float pi = 3.1416f;
+  float raio = 20;
+  float perimetro = 2 * pi * raio;
+
+  printf("o perímetro da pizza é%f\n", perimetro);
+  return 0;
+}
